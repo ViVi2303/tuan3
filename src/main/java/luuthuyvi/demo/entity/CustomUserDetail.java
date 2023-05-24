588e09ad717b2d54f714c0fbcdc099c18e8d2849
hung duc <taoxinma01@gmail.com>
2023-05-24 13:09:51 +0700

    nop bai tuan 2

---
+++ src/main/java/luuthuyvi/demo/entity/CustomUserDetail.java
@@ -0,0 +1,69 @@
+package luuthuyvi.demo.entity;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+public class CustomUserDetail implements  UserDetails{
+    private final User user;
+    public CustomUserDetail(User user){this.user = user;
+    }
+
+    /**
+     * @return
+     */
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+
+    /**
+     * @return
+     */
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    /**
+     * @return
+     */
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    /**
+     * @return
+     */
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    /**
+     * @return
+     */
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    /**
+     * @return
+     */
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    /**
+     * @return
+     */
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
